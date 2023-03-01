9748b2bc32009072437aba1040ec87ded8b8aca9
hyeonjun kim <k656000@gmail.com>
2023-03-01 16:08:16 +0900

    DB 커넥션 생성

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require("express");
-
-
+const mysql = require("mysql");
 const app = express();
 const port = 3000;
 
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//뷰파일 설정
 app.set('views','views');
-
+//뷰 템플릿 엔진 설정
 app.set("view engine", "ejs");
+
+// DB 커넥션 생성
+const connection = mysql.createConnection({
+  connectionLimit: 10,
+  host: dbconfig.host,
+  user: dbconfig.user,
+  password: dbconfig.password,
+  database: dbconfig.database,
+  debug:false
+});
+// DB 접속
+connection.connect();
 
 app.get("/", (req, res) => {
   res.render("pages/home")
 })
-
-
-
 app.get("/login", (req, res) => {
   res.render("pages/login")
 })
