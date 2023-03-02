fcd1a3ab199762b644bcf81cd72b806f909f4779
hyeonjun kim <k656000@gmail.com>
2023-03-02 16:19:24 +0900

    유저 아이디 중복 제거

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 
 const crypto = require("crypto");
-const dbconfig = require("./config/dbconfig.json")
+const dbconfig = require("./config/dbconfig.json")//데이터베이스 정보
 const app = express();
 const port = 3000;
 
@@ -54,25 +54,34 @@ app.post("/login",(req,res) => {
 app.get("/signin", (req, res) => {
   res.render("pages/signin")
 })
+
 app.post("/signin",(req,res)=>{
-  const email = req.body.email
-  const name = req.body.name
-  const age = req.body.age
-  const password = req.body.password
+
   const salt = crypto.randomBytes(32).toString('base64')// 솔트 생성
   const hashedPw = crypto.pbkdf2Sync(req.body.password, salt, 1, 32, 'sha512').toString('base64')
   const param = [req.body.email,req.body.name,req.body.age,hashedPw,salt]
-  console.log(`salt : ${salt} , hashedPW1: ${hashedPw}`)
-  connection.query('INSERT INTO `nodeapp`.`users` (`id`, `name`, `age`, `password`, `salt`) VALUES (?,?,?,?,?)',param,(err,row) =>{
-      if(err) 
-          console.log(err);
-      console.log(`${name} 회원가입 성공`)
-      res.send("<script>alert(`회원가입을 축하합니다.`);  window.location.replace('/login')</script>")
-  });
-      
-  
+  // 같은 아이디가 있는지확인
+  connection.query("SELECT * FROM `nodeapp`.`users` WHERE `id` = ? ;",[req.body.email],(err, rows) => {
+    if (err) {
+      console.log(err);
+    } 
+    else {
+      if (rows.length > 0) {
+        res.send("<script>alert(`이미 등록된 이메일입니다.`); window.location.replace('/signin');</script>");
+      } 
+      else{
+        connection.query('INSERT INTO `nodeapp`.`users` (`id`, `name`, `age`, `password`, `salt`) VALUES (?,?,?,?,?)',param,(err,row) =>{
+          if(err) 
+              console.log(err);
+          else{
+            console.log(`${req.body.name} 회원가입 성공`)
+            res.send("<script>alert(`회원가입을 축하합니다.`);  window.location.replace('/login')</script>")
+          }   
+        });
+      }
+    }
+  }); 
 });
- 
 app.get("/mypage", (req, res) => {
     res.render("pages/mypage")
 })
